453f5299535fde5d5f7ac13d62a649b762759351
Richard Blythe <unity3software@gmail.com>
2017-12-26 23:54:27 -0600

    Updated REST function to return a non json formatted url. :)

--- functions/rest-api.php
+++ functions/rest-api.php
@@ -2,9 +2,26 @@
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'clearbase/v1', '/nth-image-url/(?P<nth>\d+)', array(
 		'methods' => 'GET',
-		'callback' => 'clearbase_rest_api_nth_image_url',
+		'callback' => 'clearbase_rest_api_nth_image_url'
 	) );
 } );
+
+
+add_filter( 'rest_pre_serve_request', 'clearbase_rest_pre_serve_request', 10, 4 );
+
+function clearbase_rest_pre_serve_request( $served, $result, $request, $server ) {
+	// assumes 'format' was passed into the intial API route
+	// example: https://baconipsum.com/wp-json/baconipsum/test-response?format=text
+	// the default JSON response will be handled automatically by WP-API
+	$attributes = $request->get_attributes();
+	if ('clearbase_rest_api_nth_image_url' == $attributes['callback']) {
+		header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+		echo $result->data;
+		$served = true; // tells the WP-API that we sent the response already
+	}
+	return $served;
+}
+
 
 
 /**
@@ -17,7 +34,8 @@ function clearbase_rest_api_nth_image_url( $data ) {
 	$attachment = clearbase_get_nth_attachment('image', null, $data['nth']);
 
 	if ( !is_wp_error($attachment) )  {
-		return wp_get_attachment_image_url($attachment->ID, 'medium');
+		$str = stripslashes(wp_get_attachment_image_url($attachment->ID, 'medium'));
+		return $str;
 	}
 
 	return 'not working yet';
