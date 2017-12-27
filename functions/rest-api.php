5fb6c3861b1f0d3cbbf103dcf034b3f284168c73
Richard Blythe <unity3software@gmail.com>
2017-12-27 01:03:23 -0600

    Another update to the rest api to allow for multiple return formats

--- functions/rest-api.php
+++ functions/rest-api.php
@@ -2,7 +2,7 @@
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'clearbase/v1', '/nth-image/(?P<nth>\d+)', array(
 		'methods' => 'GET',
-		'callback' => 'clearbase_rest_api_nth_image'
+		'callback' => 'clearbase_rest_api_nth_image'//?size=[],format=['id, image, url,'title',caption']
 	) );
 } );
 
@@ -19,9 +19,8 @@ function clearbase_rest_pre_serve_request( $served, $result, $request, $server )
 //		echo $result->data;
 //		$served = true; // tells the WP-API that we sent the response already
 
-
 		$uploads = wp_upload_dir();
-
+		$format = $request->get_param('format');
 		//set a default size
 		$size = $request->get_param('size');
 		if ($size != 'full' ) {
@@ -31,23 +30,53 @@ function clearbase_rest_pre_serve_request( $served, $result, $request, $server )
 			}
 		}
 
-		// Get the image object
-		$image_object = wp_get_attachment_image_src($result->data->ID, $size );
-		// Isolate the url
-		$image_url = $image_object[0];
-		// Using the wp_upload_dir replace the baseurl with the basedir
-		$image_path = str_replace( $uploads['baseurl'], $uploads['basedir'], $image_url );
-		//replace forward slashes with back slashes
-		$image_path = str_replace('\\','/', $image_path);
 
-		$handle = fopen($image_path, "rb");
-		$contents = fread($handle, filesize($image_path));
-		fclose($handle);
+		switch ($format) {
+			case 'id':
+				header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+		        echo $result->data;
+				$served = true;
+				break;
+			case 'url':
+				// Get the image object
+				$image_object = wp_get_attachment_image_src($result->data->ID, $size );
+				// Isolate the url
+				$image_url = $image_object[0];
+				header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+		        echo $result->data;
+				$served = true;
+				break;
+			case 'title':
+				header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+				echo $result->data->post_title;
+				$served = true;
+				break;
+			case 'caption':
+				header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+				echo $result->data->post_excerpt;
+				$served = true;
+				break;
+			default:
+				// Get the image object
+				$image_object = wp_get_attachment_image_src($result->data->ID, $size );
+				// Isolate the url
+				$image_url = $image_object[0];
+				// Using the wp_upload_dir replace the baseurl with the basedir
+				$image_path = str_replace( $uploads['baseurl'], $uploads['basedir'], $image_url );
+				//replace forward slashes with back slashes
+				$image_path = str_replace('\\','/', $image_path);
 
-		header("Content-Type: image/jpeg");
+				$handle = fopen($image_path, "rb");
+				$contents = fread($handle, filesize($image_path));
+				fclose($handle);
 
-		echo $contents;
-		$served = true;
+				header("Content-Type: image/jpeg");
+
+				echo $contents;
+				$served = true;
+				break;
+		}
+
 	}
 	return $served;
 }
